0aa30ba4205fddbe93b92a28c139f41aaac3cbcd
Tom Wieland <tom.wieland@gmail.com>
2024-02-17 15:55:48 +0100

    feature(applications/testautogen): added testautogen application

---
+++ applications/testautogen/flake.nix
@@ -0,0 +1,112 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.pyproject-nix.url = "github:nix-community/pyproject.nix";
+  inputs.pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pyproject-nix,
+  } @ inputs: let
+    nixpkgsFor = nixpkgs.lib.genAttrs flake-utils.lib.defaultSystems (system:
+      import nixpkgs {
+        inherit system;
+      });
+  in (flake-utils.lib.eachDefaultSystem (
+    system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      python = pkgs.python3.override {
+        packageOverrides = self: super: {};
+      };
+
+      project = pyproject-nix.lib.project.loadPyproject {
+        projectRoot = ./.;
+      };
+
+      projectPythonPackages = project.renderers.withPackages {inherit python;};
+
+      extraPythonPackages = ps: let
+        # scikit-learn = ps.buildPythonPackage {
+        #   pname = "scikit-learn";
+        #   version = "1.4.0";
+        #   src = ps.fetchPypi {
+        #     pname = "scikit-learn";
+        #     version = "1.4.0";
+        #     sha256 = "sha256-1Dc8mE66IOOTIW7dUaPj7t5Wy+k9QkdRbSBWQ8O5MSE=";
+        #   };
+        #   buildInputs = with ps; [
+        #     numpy
+        #     pyspark
+        #     pandas
+        #     scipy
+        #     pytest
+        #   ];
+        #   nativeBuildInputs = with ps; [
+        #     cython
+        #     pytest
+        #   ];
+        # };
+        flaml = ps.buildPythonPackage {
+          pname = "FLAML";
+          version = "2.1.1";
+          src = ps.fetchPypi {
+            pname = "FLAML";
+            version = "2.1.1";
+            sha256 = "sha256-U+lKrMmW2oD+d5vGgz07JcgMd/4RZn0JEnmOSSkygus=";
+          };
+          buildInputs = with ps; [
+            numpy
+            pyspark
+            pandas
+            scipy
+            scikit-learn
+            transformers
+            tiktoken
+          ];
+        };
+
+        pyautogen = ps.buildPythonPackage {
+          pname = "pyautogen";
+          version = "0.2.13";
+          src = ps.fetchPypi {
+            pname = "pyautogen";
+            version = "0.2.13";
+            sha256 = "sha256-nsjYER/tEghgw3Lx2WKjPosPwmV8YkMy9AsSBO7B+fk=";
+          };
+          buildInputs = with ps; [
+            pip
+            docker
+            pydantic
+            tiktoken
+            python-dotenv
+            flaml
+          ];
+        };
+      in [
+        ps.numpy
+        ps.pandas
+        pyautogen
+      ];
+
+      pythonEnvironment = pkgs.python3.withPackages (
+        ps:
+          (projectPythonPackages ps) ++ (extraPythonPackages ps)
+      );
+
+      shellHook = with pkgs; ''
+        export LD_LIBRARY_PATH=${stdenv.cc.cc.lib.outPath}/lib/:$LD_LIBRARY_PATH
+      '';
+    in {
+      devShells.default = pkgs.mkShell {
+        shellHook = shellHook;
+        buildInputs = [
+          pythonEnvironment
+        ];
+      };
+    }
+  ));
+}
--- applications/testautogen/testautogen/hello.py
+++ applications/testautogen/testautogen/hello.py
@@ -1,6 +1,32 @@
-"""Sample Hello World application."""
+import os
+
+from autogen import AssistantAgent, UserProxyAgent
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def hello():
     """Return a friendly greeting."""
     return "Hello testautogen"
+
+
+# config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
+config_list = {
+    "config_list": [
+        {
+            "model": "gpt-3.5-turbo",
+            "api_key": os.getenv("OPENAI_API_KEY"),
+        }
+    ]
+}
+
+assistant = AssistantAgent("assistant", llm_config={"config_list": config_list})
+
+user_proxy = UserProxyAgent(
+    "user_proxy", code_execution_config={"work_dir": "coding", "use_docker": False}
+)
+
+user_proxy.initiate_chat(
+    assistant, message="Plot a chart of NVDA and TESLA stock price change YTD."
+)
