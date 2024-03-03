d478de2b397b057cefab3395344ffbb41e8d5628
Tom Wieland <tom.wieland@gmail.com>
2024-03-03 09:55:52 +0100

    fix(testautogen): pass packages to python and import from pyproject.toml

--- applications/testautogen/flake.nix
+++ applications/testautogen/flake.nix
@@ -11,76 +11,77 @@
   } @ inputs: (flake-utils.lib.eachDefaultSystem (
     system: let
       pkgs = import nixpkgs {inherit system;};
+      pythonPackages = pkgs.python311Packages;
 
-      extraPackages = ps: let
-        openai = pkgs.python311Packages.buildPythonPackage {
+      openai = pythonPackages.buildPythonPackage {
+        pname = "openai";
+        version = "1.13.3";
+        pyproject = true;
+        doCheck = false;
+        src = pythonPackages.fetchPypi {
           pname = "openai";
           version = "1.13.3";
-          pyproject = true;
-          doCheck = false;
-          src = pkgs.python311Packages.fetchPypi {
-            pname = "openai";
-            version = "1.13.3";
-            sha256 = "sha256-/2xrO8cyfnFeSzWSqSOlocdRn/XddkqD1p9jPUnnens=";
-          };
-          buildInputs = with pkgs.python311Packages; [
-            hatchling
-            anyio
-            distro
-            httpx
-            pydantic
-            sniffio
-            tqdm
-            typing-extensions
-          ];
+          sha256 = "sha256-/2xrO8cyfnFeSzWSqSOlocdRn/XddkqD1p9jPUnnens=";
         };
-        flaml = pkgs.python311Packages.buildPythonPackage {
+        buildInputs = with pythonPackages; [
+          hatchling
+          anyio
+          distro
+          httpx
+          pydantic
+          sniffio
+          tqdm
+          typing-extensions
+        ];
+      };
+
+      flaml = pythonPackages.buildPythonPackage {
+        pname = "FLAML";
+        version = "2.1.1";
+        pyproject = true;
+        doCheck = false;
+        src = pythonPackages.fetchPypi {
           pname = "FLAML";
           version = "2.1.1";
-          pyproject = true;
-          doCheck = false;
-          src = pkgs.python311Packages.fetchPypi {
-            pname = "FLAML";
-            version = "2.1.1";
-            sha256 = "sha256-U+lKrMmW2oD+d5vGgz07JcgMd/4RZn0JEnmOSSkygus=";
-          };
-          buildInputs = with pkgs.python311Packages; [
-            setuptools
-            numpy
-          ];
+          sha256 = "sha256-U+lKrMmW2oD+d5vGgz07JcgMd/4RZn0JEnmOSSkygus=";
         };
-        pyautogen = pkgs.python311Packages.buildPythonPackage {
+        buildInputs = with pythonPackages; [
+          setuptools
+          numpy
+        ];
+      };
+
+      pyautogen = pythonPackages.buildPythonPackage {
+        pname = "pyautogen";
+        version = "0.2.14";
+        pyproject = true;
+        doCheck = false;
+        src = pythonPackages.fetchPypi {
           pname = "pyautogen";
           version = "0.2.14";
-          pyproject = true;
-          doCheck = false;
-          src = pkgs.python311Packages.fetchPypi {
-            pname = "pyautogen";
-            version = "0.2.14";
-            sha256 = "sha256-lpMfGOnwg3zlPlKRi8nIFdm9JGONQwyQQGWE0mpWG2g=";
-          };
-          buildInputs = with pkgs.python311Packages; [
-            diskcache
-            docker
-            flaml
-            openai
-            pydantic
-            python-dotenv
-            setuptools
-            termcolor
-            tiktoken
-          ];
+          sha256 = "sha256-lpMfGOnwg3zlPlKRi8nIFdm9JGONQwyQQGWE0mpWG2g=";
         };
-      in [
-        pyautogen
-      ];
+        buildInputs = with pythonPackages; [
+          diskcache
+          docker
+          flaml
+          openai
+          pydantic
+          python-dotenv
+          setuptools
+          termcolor
+          tiktoken
+        ];
+      };
 
       python =
         pkgs
         .python311
         .override {
           packageOverrides = self: super: {
-            inherit (pkgs.python311Packages) all;
+            openai = openai;
+            flaml = flaml;
+            pyautogen = pyautogen;
           };
         };
 
@@ -90,12 +91,10 @@
 
       projectPythonPackages = project.renderers.withPackages {
         inherit python;
-        inherit extraPackages;
       };
 
       projectPythonAttributes = project.renderers.buildPythonPackage {
         inherit python;
-        extras = extraPackages;
       };
 
       pythonEnvironment = python.withPackages projectPythonPackages;
