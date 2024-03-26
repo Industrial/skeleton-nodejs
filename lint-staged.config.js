b58836de9908fc1d7220b45a3343bdab5d1efe7e
Tom Wieland <tom.wieland@gmail.com>
2024-03-26 11:56:35 +0100

    fix: remove nx format

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,5 @@
 export default {
   '{applications,packages}/**/*': [
-    `nx format:write --uncommitted`,
     `nx affected --uncommitted --target lint`,
     files => `git add ${files.join(' ')}`,
   ],
