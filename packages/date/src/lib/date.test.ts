de98be8d98aba4483d5dc23a50724300d49bf063
Tom Wieland <tom.wieland@gmail.com>
2024-06-30 14:00:37 +0200

    feat(applications/debank-scraper): sucessfully import into db

--- packages/date/src/lib/date.test.ts
+++ packages/date/src/lib/date.test.ts
@@ -1,26 +1,112 @@
-import { describe, expect, test } from 'bun:test'
-import { Option } from 'effect'
+import { describe, expect, it } from 'bun:test'
+import { Option as O, pipe } from 'effect'
 
 import { convertTime } from './date.ts'
 
-describe('Time Conversion with convertTime Function', () => {
-  test('Convert milliseconds to seconds', () => {
-    expect(convertTime(1000, 'ms', 's')).toStrictEqual(Option.some(1))
-    expect(convertTime(5000, 'ms', 's')).toStrictEqual(Option.some(5))
+export type TimeUnit = 'd' | 'h' | 'm' | 'ms' | 's'
+
+describe('convertTime', () => {
+  describe('When converting from ms to s', () => {
+    it('should convert 1000 milliseconds to 1 second', () => {
+      const actual = pipe(
+        O.fromNullable(1000),
+        O.flatMap(convertTime('ms', 's')),
+      )
+      if (O.isNone(actual)) {
+        throw new Error('None')
+      }
+      expect(actual.value).toStrictEqual(1)
+    })
   })
 
-  test('Convert seconds to milliseconds', () => {
-    expect(convertTime(1, 's', 'ms')).toStrictEqual(Option.some(1000))
-    expect(convertTime(10, 's', 'ms')).toStrictEqual(Option.some(10000))
+  describe('When converting from s to m', () => {
+    it('should convert 60 seconds to 1 minute', () => {
+      const actual = pipe(
+        O.fromNullable(60),
+        O.flatMap(convertTime('s', 'm')),
+      )
+      if (O.isNone(actual)) {
+        throw new Error('None')
+      }
+      expect(actual.value).toStrictEqual(1)
+    })
   })
 
-  test('Convert minutes to hours', () => {
-    expect(convertTime(60, 'm', 'h')).toStrictEqual(Option.some(1))
-    expect(convertTime(120, 'm', 'h')).toStrictEqual(Option.some(2))
+  describe('When converting from m to h', () => {
+    it('should convert 60 minutes to 1 hour', () => {
+      const actual = pipe(
+        O.fromNullable(60),
+        O.flatMap(convertTime('m', 'h')),
+      )
+      if (O.isNone(actual)) {
+        throw new Error('None')
+      }
+      expect(actual.value).toStrictEqual(1)
+    })
   })
 
-  test('Convert hours to days', () => {
-    expect(convertTime(24, 'h', 'd')).toStrictEqual(Option.some(1))
-    expect(convertTime(48, 'h', 'd')).toStrictEqual(Option.some(2))
+  describe('When converting from h to d', () => {
+    it('should convert 24 hours to 1 day', () => {
+      const actual = pipe(
+        O.fromNullable(24),
+        O.flatMap(convertTime('h', 'd')),
+      )
+      if (O.isNone(actual)) {
+        throw new Error('None')
+      }
+      expect(actual.value).toStrictEqual(1)
+    })
+  })
+
+  describe('When converting from d to h', () => {
+    it('should convert 1 days to 24 hours', () => {
+      const actual = pipe(
+        O.fromNullable(1),
+        O.flatMap(convertTime('d', 'h')),
+      )
+      if (O.isNone(actual)) {
+        throw new Error('None')
+      }
+      expect(actual.value).toStrictEqual(24)
+    })
+  })
+
+  describe('When converting from h to m', () => {
+    it('should convert 1 hour to 60 minutes', () => {
+      const actual = pipe(
+        O.fromNullable(1),
+        O.flatMap(convertTime('h', 'm')),
+      )
+      if (O.isNone(actual)) {
+        throw new Error('None')
+      }
+      expect(actual.value).toStrictEqual(60)
+    })
+  })
+
+  describe('When converting from m to s', () => {
+    it('should convert 60 minutes to 3600 seconds', () => {
+      const actual = pipe(
+        O.fromNullable(60),
+        O.flatMap(convertTime('m', 's')),
+      )
+      if (O.isNone(actual)) {
+        throw new Error('None')
+      }
+      expect(actual.value).toStrictEqual(3600)
+    })
+  })
+
+  describe('When converting from s to ms', () => {
+    it('should convert 1 seconds to 1000 milliseconds', () => {
+      const actual = pipe(
+        O.fromNullable(1),
+        O.flatMap(convertTime('s', 'ms')),
+      )
+      if (O.isNone(actual)) {
+        throw new Error('None')
+      }
+      expect(actual.value).toStrictEqual(1000)
+    })
   })
 })
--- packages/log/src/lib/log.ts
+++ packages/log/src/lib/log.ts
@@ -1,3 +1,6 @@
 import { Logger } from 'tslog'
 
-export const log = new Logger()
+// 0: silly, 1: trace, 2: debug, 3: info, 4: warn, 5: error, 6: fatal
+export const log = new Logger({
+  minLevel: 2,
+})
