48b6ca2ea49882b1eb77decc7ce642d3696052a0
ss-twieland <twieland@suitsupply.com>
2024-10-11 09:05:58 +0200

    feat(test-leptos): leptos is running

--- applications/test-leptos/flake.nix
+++ applications/test-leptos/flake.nix
@@ -17,149 +17,168 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, crane, fenix, flake-utils, advisory-db, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    fenix,
+    flake-utils,
+    advisory-db,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
 
-        craneLib = (crane.mkLib pkgs).overrideToolchain (p: p.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
-          targets = [
-            "wasm32-wasi"
-          ];
-        }));
+      craneLib = (crane.mkLib pkgs).overrideToolchain (p:
+        p.rust-bin.selectLatestNightlyWith (toolchain:
+          toolchain.default.override {
+            targets = [
+              "wasm32-unknown-unknown"
+            ];
+          }));
 
-        src = craneLib.cleanCargoSource ./.;
-        # Common arguments can be set here to avoid repeating them later
-        commonArgs = {
-          inherit src;
-          strictDeps = true;
-          # Tests currently need to be run via `cargo wasi` which
-          # isn't packaged in nixpkgs yet...
-          doCheck = false;
-          cargoExtraArgs = "--target wasm32-wasi";
-          buildInputs = [
+      src = craneLib.cleanCargoSource ./.;
+      # Common arguments can be set here to avoid repeating them later
+      commonArgs = {
+        inherit src;
+        strictDeps = true;
+        # Tests currently need to be run via `cargo wasi` which
+        # isn't packaged in nixpkgs yet...
+        doCheck = false;
+        cargoExtraArgs = "--target wasm32-wasi";
+        buildInputs =
+          [
             # Add additional build inputs here
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
             # Additional darwin specific inputs can be set here
             pkgs.libiconv
           ];
-          # Additional environment variables can be set directly
-          # MY_CUSTOM_VAR = "some value";
-        };
+        # Additional environment variables can be set directly
+        # MY_CUSTOM_VAR = "some value";
+      };
 
-        # TODO: Understand why we are creating a LLVM version.
-        craneLibLLvmTools = craneLib.overrideToolchain
-          (fenix.packages.${system}.complete.withComponents [
-            "cargo"
-            "llvm-tools"
-            "rustc"
-          ]);
+      # TODO: Understand why we are creating a LLVM version.
+      craneLibLLvmTools =
+        craneLib.overrideToolchain
+        (fenix.packages.${system}.complete.withComponents [
+          "cargo"
+          "llvm-tools"
+          "rustc"
+        ]);
 
-        # Build *just* the cargo dependencies, so we can reuse
-        # all of that work (e.g. via cachix) when running in CI
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+      # Build *just* the cargo dependencies, so we can reuse
+      # all of that work (e.g. via cachix) when running in CI
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-        # Build the actual crate itself, reusing the dependency
-        # artifacts from above.
-        my-crate = craneLib.buildPackage (commonArgs // {
+      # Build the actual crate itself, reusing the dependency
+      # artifacts from above.
+      my-crate = craneLib.buildPackage (commonArgs
+        // {
           inherit cargoArtifacts;
         });
-
-        # my-crate = craneLib.buildPackage {
-        #   inherit src;
-        #   strictDeps = true;
-        #   cargoVendorDir = craneLib.vendorMultipleCargoDeps {
-        #     inherit (craneLib.findCargoFiles src) cargoConfigs;
-        #     cargoLockList = [
-        #       ./Cargo.lock
-        #       # Unfortunately this approach requires IFD (import-from-derivation)
-        #       # otherwise Nix will refuse to read the Cargo.lock from our toolchain
-        #       # (unless we build with `--impure`).
-        #       #
-        #       # Another way around this is to manually copy the rustlib `Cargo.lock`
-        #       # to the repo and import it with `./path/to/rustlib/Cargo.lock` which
-        #       # will avoid IFD entirely but will require manually keeping the file
-        #       # up to date!
-        #       "${rustToolchain.passthru.availableComponents.rust-src}/lib/rustlib/src/rust/library/Cargo.lock"
-        #     ];
-        #   };
-        #   cargoExtraArgs = "-Z build-std --target x86_64-unknown-linux-gnu";
-        #   buildInputs = [
-        #     # Add additional build inputs here
-        #   ];
-        # };
-      in
-      {
-        checks = {
-          # Build the crate as part of `nix flake check` for convenience
-          inherit my-crate;
-          # Run clippy (and deny all warnings) on the crate source,
-          # again, reusing the dependency artifacts from above.
-          #
-          # Note that this is done as a separate derivation so that
-          # we can block the CI if there are issues here, but not
-          # prevent downstream consumers from building our crate by itself.
-          my-crate-clippy = craneLib.cargoClippy (commonArgs // {
+      # my-crate = craneLib.buildPackage {
+      #   inherit src;
+      #   strictDeps = true;
+      #   cargoVendorDir = craneLib.vendorMultipleCargoDeps {
+      #     inherit (craneLib.findCargoFiles src) cargoConfigs;
+      #     cargoLockList = [
+      #       ./Cargo.lock
+      #       # Unfortunately this approach requires IFD (import-from-derivation)
+      #       # otherwise Nix will refuse to read the Cargo.lock from our toolchain
+      #       # (unless we build with `--impure`).
+      #       #
+      #       # Another way around this is to manually copy the rustlib `Cargo.lock`
+      #       # to the repo and import it with `./path/to/rustlib/Cargo.lock` which
+      #       # will avoid IFD entirely but will require manually keeping the file
+      #       # up to date!
+      #       "${rustToolchain.passthru.availableComponents.rust-src}/lib/rustlib/src/rust/library/Cargo.lock"
+      #     ];
+      #   };
+      #   cargoExtraArgs = "-Z build-std --target x86_64-unknown-linux-gnu";
+      #   buildInputs = [
+      #     # Add additional build inputs here
+      #   ];
+      # };
+    in {
+      checks = {
+        # Build the crate as part of `nix flake check` for convenience
+        inherit my-crate;
+        # Run clippy (and deny all warnings) on the crate source,
+        # again, reusing the dependency artifacts from above.
+        #
+        # Note that this is done as a separate derivation so that
+        # we can block the CI if there are issues here, but not
+        # prevent downstream consumers from building our crate by itself.
+        my-crate-clippy = craneLib.cargoClippy (commonArgs
+          // {
             inherit cargoArtifacts;
             cargoClippyExtraArgs = "--all-targets -- --deny warnings";
           });
-          my-crate-doc = craneLib.cargoDoc (commonArgs // {
+        my-crate-doc = craneLib.cargoDoc (commonArgs
+          // {
             inherit cargoArtifacts;
           });
-          # Check formatting
-          my-crate-fmt = craneLib.cargoFmt {
-            inherit src;
-          };
-          my-crate-toml-fmt = craneLib.taploFmt {
-            src = pkgs.lib.sources.sourceFilesBySuffices src [ ".toml" ];
-            # taplo arguments can be further customized below as needed
-            # taploExtraArgs = "--config ./taplo.toml";
-          };
-          # Audit dependencies
-          my-crate-audit = craneLib.cargoAudit {
-            inherit src advisory-db;
-          };
-          # Audit licenses
-          my-crate-deny = craneLib.cargoDeny {
-            inherit src;
-          };
-          # Run tests with cargo-nextest
-          # Consider setting `doCheck = false` on `my-crate` if you do not want
-          # the tests to run twice
-          my-crate-nextest = craneLib.cargoNextest (commonArgs // {
+        # Check formatting
+        my-crate-fmt = craneLib.cargoFmt {
+          inherit src;
+        };
+        my-crate-toml-fmt = craneLib.taploFmt {
+          src = pkgs.lib.sources.sourceFilesBySuffices src [".toml"];
+          # taplo arguments can be further customized below as needed
+          # taploExtraArgs = "--config ./taplo.toml";
+        };
+        # Audit dependencies
+        my-crate-audit = craneLib.cargoAudit {
+          inherit src advisory-db;
+        };
+        # Audit licenses
+        my-crate-deny = craneLib.cargoDeny {
+          inherit src;
+        };
+        # Run tests with cargo-nextest
+        # Consider setting `doCheck = false` on `my-crate` if you do not want
+        # the tests to run twice
+        my-crate-nextest = craneLib.cargoNextest (commonArgs
+          // {
             inherit cargoArtifacts;
             partitions = 1;
             partitionType = "count";
           });
-        };
-        packages = {
+      };
+      packages =
+        {
           default = my-crate;
-        } // pkgs.lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+        }
+        // pkgs.lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
           # TODO: Understand why we are creating a LLVM version.
-          my-crate-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs // {
-            inherit cargoArtifacts;
-          });
+          my-crate-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs
+            // {
+              inherit cargoArtifacts;
+            });
         };
-        # apps.default = flake-utils.lib.mkApp {
-        #   drv = my-crate;
-        # };
-        apps.default = flake-utils.lib.mkApp {
-          drv = pkgs.writeShellScriptBin "my-app" ''
-            ${pkgs.wasmtime}/bin/wasmtime run ${my-crate}/bin/custom-toolchain.wasm
-          '';
-        };
-        devShells.default = craneLib.devShell {
-          # Inherit inputs from checks.
-          checks = self.checks.${system};
-          # Additional dev-shell environment variables can be set directly
-          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
-          # Extra inputs can be added here; cargo and rustc are provided by default.
-          packages = [
-            # pkgs.ripgrep
-          ];
-        };
-      });
+      # apps.default = flake-utils.lib.mkApp {
+      #   drv = my-crate;
+      # };
+      apps.default = flake-utils.lib.mkApp {
+        drv = pkgs.writeShellScriptBin "my-app" ''
+          ${pkgs.wasmtime}/bin/wasmtime run ${my-crate}/bin/custom-toolchain.wasm
+        '';
+      };
+      devShells.default = craneLib.devShell {
+        # Inherit inputs from checks.
+        checks = self.checks.${system};
+        # Additional dev-shell environment variables can be set directly
+        # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+        # Extra inputs can be added here; cargo and rustc are provided by default.
+        packages = with pkgs; [
+          trunk
+          # pkgs.ripgrep
+        ];
+      };
+    });
 }
