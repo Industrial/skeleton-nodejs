1cad8d92c1501c0781fd2084af28439e2a6c7f41
tom <tom@x86_64-linux.local>
2025-01-26 17:57:58 +0000

    feat(test-solid-start-bun-effect): lint and types

--- applications/test-solid-start-bun-effect/src/app.tsx
+++ applications/test-solid-start-bun-effect/src/app.tsx
@@ -1,22 +1,26 @@
-import { createSignal } from "solid-js";
-import "./app.css";
+import { createSignal } from 'solid-js'
+import './app.css'
 
 export default function App() {
-  const [count, setCount] = createSignal(0);
+  const [count, setCount] = createSignal(0)
 
   return (
     <main>
       <h1>Hello world!</h1>
-      <button class="increment" onClick={() => setCount(count() + 1)} type="button">
+      <button
+        class="increment"
+        onClick={() => setCount(count() + 1)}
+        type="button"
+      >
         Clicks: {count()}
       </button>
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
+        Visit{' '}
+        <a href="https://start.solidjs.com" target="_blank" rel="noreferrer">
           start.solidjs.com
-        </a>{" "}
+        </a>{' '}
         to learn how to build SolidStart apps.
       </p>
     </main>
-  );
+  )
 }
--- applications/test-solid-start-bun-effect/src/entry-client.tsx
+++ applications/test-solid-start-bun-effect/src/entry-client.tsx
@@ -1,4 +1,11 @@
 // @refresh reload
-import { mount, StartClient } from "@solidjs/start/client";
+import { StartClient, mount } from '@solidjs/start/client'
 
-mount(() => <StartClient />, document.getElementById("app")!);
+const appElementId = 'app'
+const appElement = document.getElementById(appElementId)
+
+if (!appElement) {
+  throw new Error(`No element with id "${appElementId}" found.`)
+}
+
+mount(() => <StartClient />, appElement)
